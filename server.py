6365c871f172d1139c75e701dfb5f6cf934d7ee6
Anjelica S <anjelicacsilva@gmail.com>
2021-12-10 16:41:07 +0000

    Create basic server
    Co-authored-by: Anjelica Silva <anjelicacsilva@gmail.com>
    Co-authored-by: Katrina Huber-Juma <katrina.huber@gmail.com>

---
+++ server.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    """Show homepage"""
+
+    return render_template('base.html')
+
+
+
+if __name__ == '__main__':
+    
+    # connect_to_db(app)
+    app.run(host='0.0.0.0', port='5000')
